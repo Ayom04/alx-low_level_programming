e4734f163d917c759fde275be8ebca1b34181c99
Ayom04 <abdullahayomide04@gmail.com>
2022-12-11 09:45:52 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ * main - returns alphabets
+ * Return: Always 0
+*/
+int main (void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
