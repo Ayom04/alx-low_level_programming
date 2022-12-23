e96336ce428ccaf5bd6a21ea7f51651502df3d74
Ayom04 <abdullahayomide04@gmail.com>
2022-12-22 18:26:18 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include "maun.h"
+#include "main.h"
+
 /**
- * _strncat - Function that concatenates two strings.
+ * _strncat - Function that concatenates two strings
  * @dest: first string
  * @src: second string
- *  @n: number of bytes to concatenate
- *  Return: pointer to designated string
+ * @n: number of bytes to concatenate
+ * Return: pointer to designated string
 */
 char *_strncat(char *dest, char *src, int n)
 {
